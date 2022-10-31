75537fe123bc097477e498e12ea44b37025aa042
Z3belek <you@example.com>
2022-10-31 18:19:37 -0300

    Material y bootstrap agregados

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,7 @@
 
 export const environment = {
   production: false,
+  apiBase: 'http://wallet-main.eba-ccwdurgr.us-east-1.elasticbeanstalk.com'
 };
 /*
  * For easier debugging in development mode, you can import the following file
