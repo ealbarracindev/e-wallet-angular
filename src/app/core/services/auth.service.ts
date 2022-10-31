352257bd1e7b6712ef9ec59046d192214729065b
Z3belek <you@example.com>
2022-10-31 18:23:10 -0300

    Added bootstrap and material

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,49 +1,10 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map, Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  private authUrl = `${environment.apiBase}/auth/login`;
-  private authMe = `${environment.apiBase}/auth/me`;
+  constructor() { }
 
-  constructor(
-    private http: HttpClient
-  ) { }
-
-  loginJWT(username: string, password: string): Observable<any> {
-    return this.http.post<any>(this.authUrl, { username, password })
-      .pipe(map(user => {
-        if (user && user.token) {
-          localStorage.setItem('currentUser', JSON.stringify(user));
-        }
-        return user;
-      }));
-  }
-
-  haveToken(): boolean {
-    return !!localStorage.getItem('currentUser');
-  }
-
-  logout(): void {
-    localStorage.removeItem('currentUser');
-  }
-
-  getUserInfo(): Observable<any> {
-    return this.http.get<any>(this.authMe);
-  }
-
-}
-
-export interface User {
-  first_name: string;
-  last_name: string;
-  email: string;
-  password: string;
-  roleId: number;
-  points: number;
 }
